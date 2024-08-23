2a7f0fa52377e3aaa3d8617d1d786aa5e05ecd0a
Aziz21120 <doukali_fr@live.fr>
2024-08-23 10:59:54 +0200

    correction in admin.py : deleted too much "@" in decorator

--- visits/admin.py
+++ visits/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from visits.models import PageVisit
 
 
-@@admin.register(PageVisit)
+@admin.register(PageVisit)
 class PageVisitsAdmin(admin.ModelAdmin):
     class Meta:
         model = PageVisit
